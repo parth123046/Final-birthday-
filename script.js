8dad8750ab9011ed9657e6993fc628bdce6104e2
parth123046 <147490278+parth123046@users.noreply.github.com>
2024-02-01 13:45:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const birthdays = [
         { name: 'John Doe', birthday: '1-05', class: 'X', section: 'A' },
-        { name: 'Jane Smith', birthday: '1-02', class: 'XI', section: 'B' },
-        { name: 'Michael Johnson', birthday: '1-03, class: 'XII', section: 'C' }
+        { name: 'Jane Smith', birthday: '01-02', class: 'XI', section: 'B' },
+        { name: 'Michael Johnson', birthday: ' 02-01, class: 'XII', section: 'C' }
     ];
 
     checkBirthdays(birthdays);
