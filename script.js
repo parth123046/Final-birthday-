7da06faac24fed9068f5af9b275815306b79b2b4
shantanusonawane30 <157087036+shantanusonawane30@users.noreply.github.com>
2024-02-02 15:46:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ const students = [
   { name: "Alice", birthday: "01/02" },
   { name: "Bob", birthday: "03/04" },
   { name: "Charlie", birthday: "05/06" },
-  // Add more students here
+  { name: "AARVIKA KSHITIJ TARGE", birthday: "02-03" },
+{ name: "ANAYRA ADITYA VENUGOPAL", birthday: "19-02" },
+{ name: "HARSHIT MANISH TATIYA", birthday: "21-04" },
+{ name: "TASHVIKA AKASH SACHDEV", birthday: "01-02" },
+{ name: "AARYA YASHWANT SONAWANE", birthday: "08-01" },
+{ name: "ANUP AMOL GITE", birthday: "20-04" },
+{ name: "GARGI HANSRAJ DEORE", birthday: "27-05" },
+{ name: "HRIDYA SUMIT CHORDIYA", birthday: "15-12" },
+{ name: "KAVISH PRAVIN GITE", birthday: "10-01" },
+{ name: "KAVYANSH DHANRAJ DHATRAK", birthday: "06-03" },// Add more students here
 ];
 
 // Get today's date
